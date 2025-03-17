994512d1bc810b25bc4286fd99c65fb5e2600155
adkeaina <76181539+adkeaina@users.noreply.github.com>
2025-03-17 15:32:46 -0600

    more dubs

--- backend/Amazon.API/Controllers/BookController.cs
+++ backend/Amazon.API/Controllers/BookController.cs
@@ -8,9 +8,28 @@ namespace Amazon.API.Controllers;
 public class BookController : ControllerBase
 {
     private BookstoreContext _context;
-    
+
     public BookController(BookstoreContext context) => _context = context;
-    
+
     [HttpGet("AllBooks")]
-    public IEnumerable<Book> GetBooks() => _context.Books.ToList();
+    public IActionResult GetBooks(int pageSize = 10, int pageNumber = 1)
+    {
+        var booklist = _context.Books
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        var bookCount = _context.Books.Count();
+
+        var pageCount = (int)Math.Ceiling((double)bookCount / pageSize);
+
+        var someObject = new
+        {
+            Books = booklist,
+            BookCount = bookCount,
+            PageCount = pageCount
+        };
+
+        return Ok(someObject);
+    }
 }
